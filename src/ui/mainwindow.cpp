2a6b9f9fc2334ab0f436dc2b291185bb1c79c170
fverco <francov2468@gmail.com>
2021-09-25 13:10:34 +0200

    Add splitter for adjusting sizes of the two views.

--- src/ui/mainwindow.cpp
+++ src/ui/mainwindow.cpp
@@ -1,11 +1,18 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+#include <QSplitter>
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    QSplitter *viewSplitter(new QSplitter(this));
+    ui->hLayoutFiles->addWidget(viewSplitter);
+    viewSplitter->addWidget(ui->tvFileSys);
+    viewSplitter->addWidget(ui->lvCurFolder);
 }
 
 MainWindow::~MainWindow()
