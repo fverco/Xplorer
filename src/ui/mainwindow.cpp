0888cac7cd082c4922015e82c4d9bf6f3ce10447
fverco <francov2468@gmail.com>
2021-09-26 19:02:00 +0200

    Create file models for each view and restructure code

--- src/ui/mainwindow.cpp
+++ src/ui/mainwindow.cpp
@@ -1,10 +1,12 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include "../types/explorersplitter.h"
 
 #include <QSplitter>
 #include <QFileSystemModel>
 #include <QStandardPaths>
 #include <QtDebug>
+#include <QFileIconProvider>
 
 /*!
  * \brief The constructor of the main window.
@@ -13,27 +15,12 @@
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent),
       ui(new Ui::MainWindow),
-      fileModel(new QFileSystemModel(this))
+      dirModel(new QFileSystemModel(this)),
+      fileModel1(new QFileSystemModel(this)),
+      fileModel2(new QFileSystemModel(this))
 {
     ui->setupUi(this);
-
-    // Add splitter between the file views.
-    QSplitter *viewSplitter(new QSplitter(this));
-    ui->hLayoutFiles->addWidget(viewSplitter);
-    viewSplitter->addWidget(ui->tvFileSys);
-    viewSplitter->addWidget(ui->gbExplorer1);
-    viewSplitter->addWidget(ui->gbExplorer2);
-
-    // Add file model to the file views.
-    fileModel->setRootPath(QDir::homePath());
-    fileModel->setFilter(QDir::AllEntries | QDir::NoDotAndDotDot);
-    ui->tvFileSys->setModel(fileModel);
-    ui->tvFileSys->setRootIndex(fileModel->index(QDir::homePath()));
-    ui->lvExplorer1->setModel(fileModel);
-    ui->lvExplorer1->setRootIndex(fileModel->index(QDir::homePath()));
-    ui->lvExplorer2->setModel(fileModel);
-    ui->lvExplorer2->setRootIndex(fileModel->index(QDir::homePath()));
-
+    initializeExplorerUi();
 }
 
 /*!
@@ -43,4 +30,42 @@ MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+/*!
+ * \brief Initializes the core UI components for the file explorers.
+ */
+void MainWindow::initializeExplorerUi()
+{
+    // Add splitter between the file views.
+    ExplorerSplitter *viewSplitter(new ExplorerSplitter(Qt::Horizontal, this));
+    ui->hLayoutFiles->addWidget(viewSplitter);
+    viewSplitter->addWidget(ui->tvFileSys);
+    viewSplitter->addWidget(ui->gbExplorer1);
+    viewSplitter->addWidget(ui->gbExplorer2);
+
+    // Add file models to the file views.
+    dirModel->setRootPath(QDir::homePath());
+    dirModel->setFilter(QDir::AllDirs | QDir::NoDotAndDotDot);
+    ui->tvFileSys->setModel(dirModel);
+    ui->tvFileSys->setRootIndex(dirModel->index(QDir::homePath()));
+
+    fileModel1->setRootPath(QDir::homePath());
+    fileModel1->setFilter(QDir::AllEntries | QDir::NoDot);
+    ui->lvExplorer1->setModel(fileModel1);
+    ui->lvExplorer1->setRootIndex(fileModel1->index(QDir::homePath()));
+
+    fileModel2->setRootPath(QDir::homePath());
+    fileModel2->setFilter(QDir::AllEntries | QDir::NoDot);
+    ui->lvExplorer2->setModel(fileModel2);
+    ui->lvExplorer2->setRootIndex(fileModel2->index(QDir::homePath()));
+
+    // Assign current directory of each explorer to their combo boxes.
+    ui->cbPathExplorer1->addItem(fileModel1->rootPath());
+    ui->cbPathExplorer2->addItem(fileModel2->rootPath());
+    connect(fileModel1, &QFileSystemModel::rootPathChanged, ui->cbPathExplorer1, [this](){
+                ui->cbPathExplorer1->setCurrentText(fileModel1->rootPath());
+            });
+
+}
+
 
--- src/ui/mainwindow.h
+++ src/ui/mainwindow.h
@@ -20,8 +20,15 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void on_lvExplorer1_doubleClicked(const QModelIndex &index);
+
 private:
-    Ui::MainWindow *ui;             ///< The object containing all the ui elements.
-    QFileSystemModel *fileModel;    ///< The model that retrieves information about the file system.
+    Ui::MainWindow *ui;              ///< The object containing all the ui elements.
+    QFileSystemModel *dirModel;      ///< The tree view's directory model.
+    QFileSystemModel *fileModel1;    ///< Explorer 1's file model.
+    QFileSystemModel *fileModel2;    ///< Explorer 2's file model.
+
+    void initializeExplorerUi();
 };
 #endif // MAINWINDOW_H
